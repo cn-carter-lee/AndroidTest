85d3f23f92b1780af9144fcdacd78dc29eca3f64
CarterLee <cli@xogrp.com>
2014-07-11 12:31:36 +0800

    ...

---
+++ HelloGridView/src/com/pys/hellogridview/HomeActivity.java
@@ -0,0 +1,155 @@
+package com.pys.hellogridview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar.Tab;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeActivity extends ActionBarActivity implements
+		ArtistFragment.OnFragmentInteractionListener,
+		AlbumFragment.OnFragmentInteractionListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setCustomView(R.layout.gift_send_actonbar);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		Tab tab = actionBar
+				.newTab()
+				.setText(R.string.gift_all)
+				.setTabListener(
+						new MyTabListener<ArtistFragment>(this, "album",
+								ArtistFragment.class));
+		actionBar.addTab(tab);
+
+		tab = actionBar
+				.newTab()
+				.setText(R.string.gift_male)
+				.setTabListener(
+						new MyTabListener<AlbumFragment>(this, "album",
+								AlbumFragment.class));
+		actionBar.addTab(tab);
+
+		tab = actionBar
+				.newTab()
+				.setText(R.string.gift_female)
+				.setTabListener(
+						new MyTabListener<AlbumFragment>(this, "album",
+								AlbumFragment.class));
+		actionBar.addTab(tab);
+
+		// setContentView(R.layout.activity_home);
+
+		/*
+		 * if (savedInstanceState == null) {
+		 * getSupportFragmentManager().beginTransaction() .add(R.id.container,
+		 * new PlaceholderFragment()).commit(); }
+		 */
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		/*
+		switch (id) {
+		case R.id.action_gift_home:
+			break;
+		case R.id.action_gift_search:
+			break;
+		case R.id.action_gift_favorite:
+			break;
+		case R.id.action_gift_more:
+			break;
+		}
+		*/
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onFragmentInteraction(Uri uri) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_home, container,
+					false);
+			return rootView;
+		}
+	}
+
+	public static class MyTabListener<T extends Fragment> implements
+			ActionBar.TabListener {
+		private Fragment mFragment;
+		private final Activity mActivity;
+		private final String mTag;
+		private final Class<T> mClass;
+
+		public MyTabListener(Activity activity, String tag, Class<T> clz) {
+			this.mActivity = activity;
+			this.mTag = tag;
+			this.mClass = clz;
+		}
+
+		@Override
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+			if (mFragment == null) {
+				mFragment = Fragment.instantiate(mActivity, mClass.getName());
+				ft.add(android.R.id.content, mFragment, mTag);
+			} else {
+				ft.attach(mFragment);
+			}
+
+		}
+
+		@Override
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+			if (mFragment != null) {
+				// Detach the fragment, because another one is being attached
+				ft.detach(mFragment);
+			}
+
+		}
+
+		@Override
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
+			// TODO Auto-generated method stub
+		}
+	}
+
+}
