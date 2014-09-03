866aec1d5eeafecac2a5861abc457eb6e01702ce
CarterLee <cli@xogrp.com>
2014-09-03 09:41:09 +0800

    ...

--- HelloGridView/src/com/liwuso/app/AppStart.java
+++ HelloGridView/src/com/liwuso/app/AppStart.java
@@ -20,7 +20,7 @@ public class AppStart extends Activity {
 		setContentView(view);
 
 		AlphaAnimation aa = new AlphaAnimation(0.3f, 1.0f);
-		aa.setDuration(4000);
+		aa.setDuration(100);
 		view.startAnimation(aa);
 		aa.setAnimationListener(new AnimationListener() {
 			@Override
