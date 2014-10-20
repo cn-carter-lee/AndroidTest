808163d7ce5986aeffb99e164ee8617fcd20115f
CarterLee <cli@xogrp.com>
2014-10-20 13:56:35 +0800

    1.0 version

--- HelloGridView/src/com/liwuso/app/widget/CustomImageView.java
+++ HelloGridView/src/com/liwuso/app/widget/CustomImageView.java
@@ -1,14 +1,34 @@
 package com.liwuso.app.widget;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.ImageView;
 
 public class CustomImageView extends ImageView {
 
 	public CustomImageView(Context context) {
 		super(context);
+
 		// TODO Auto-generated constructor stub
+	}
+
+	public CustomImageView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomImageView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
 	}
 
 	@Override
@@ -19,22 +39,29 @@ public class CustomImageView extends ImageView {
 			if (drawable == null) {
 				setMeasuredDimension(0, 0);
 			} else {
-				float imageSideRatio = (float) drawable.getIntrinsicWidth()
-						/ (float) drawable.getIntrinsicHeight();
-				float viewSideRatio = (float) MeasureSpec
-						.getSize(widthMeasureSpec)
-						/ (float) MeasureSpec.getSize(heightMeasureSpec);
-				if (imageSideRatio >= viewSideRatio) {
-					// Image is wider than the display (ratio)
-					int width = MeasureSpec.getSize(widthMeasureSpec);
-					int height = (int) (width / imageSideRatio);
+				int measuredWidth = MeasureSpec.getSize(widthMeasureSpec);
+				int measuredHeight = MeasureSpec.getSize(heightMeasureSpec);
+				if (measuredHeight == 0 && measuredWidth == 0) {
+					// Height and width set to wrap_content
+					setMeasuredDimension(measuredWidth, measuredHeight);
+				} else if (measuredHeight == 0) {
+					// Height set to wrap_content
+					int width = measuredWidth;
+					int height = width * drawable.getIntrinsicHeight()
+							/ drawable.getIntrinsicWidth();
+					setMeasuredDimension(width, height);
+				} else if (measuredWidth == 0) {
+					// Width set to wrap_content
+					int height = measuredHeight;
+					int width = height * drawable.getIntrinsicWidth()
+							/ drawable.getIntrinsicHeight();
 					setMeasuredDimension(width, height);
 				} else {
-					// Image is taller than the display (ratio)
-					int height = MeasureSpec.getSize(heightMeasureSpec);
-					int width = (int) (height * imageSideRatio);
-					setMeasuredDimension(width, height);
+					// Width and height are explicitly set (either to
+					// match_parent or to exact value)
+					setMeasuredDimension(measuredWidth, measuredHeight);
 				}
+
 			}
 		} catch (Exception e) {
 			super.onMeasure(widthMeasureSpec, heightMeasureSpec);
